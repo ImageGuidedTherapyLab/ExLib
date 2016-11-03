e708829d947df88fe1e3129573df4fa6314248d0
David Fuentes <fuentesdt@gmail.com>
2016-11-03 18:25:20 -0500

    ENH: path lm

--- PathologyLandmarks/convertsvs.py
+++ PathologyLandmarks/convertsvs.py
@@ -23,22 +23,23 @@ if (options.svsfile != None and options.svsfile.split('.').pop() == 'svs' and op
      headerProcess = subprocess.Popen(getHeaderCmd ,shell=True,stdout=subprocess.PIPE )
      while ( headerProcess.poll() == None ):
         pass
-     layerinfo = dict([tuple(line.strip('\n').split(":",1)) for line in headerProcess.stdout if line[:15] =='openslide.layer'])
-     print layerinfo 
-     print layerinfo['openslide.layer[2].height'] 
-     print layerinfo['openslide.layer[2].width'] 
-     print layerinfo['openslide.layer[2].downsample'] 
+     layerinfo = dict([tuple(line.strip('\n').split(":",1)) for line in headerProcess.stdout if (line[:15] =='openslide.layer' or line[:10] =='aperio.MPP')])
+     print layerinfo
 
      #centroid = eval( rawcentroidinfo.replace('CENTROID_VOX',''))
      #print centroid , int(centroid [2])
-     width = 10
-     height = 100
+     downsample = float(layerinfo['openslide.layer[%d].downsample' % options.layer].replace("'",'')  )
+     micronperpixel = float(layerinfo['aperio.MPP' ].replace("'",'')  )
+     width      = int(  layerinfo['openslide.layer[%d].width'  % options.layer].replace("'",'') )
+     height     = int(  layerinfo['openslide.layer[%d].height' % options.layer].replace("'",'') )
+     spacing = downsample * micronperpixel  * .001
      pngfile = options.svsfile.replace('.svs','.png')
      pngcmd = 'openslide-write-png %s 0 0 %d %d %d %s ' %(options.svsfile,options.layer,width,height,pngfile )
      print pngcmd
-     #os.system(pngcmd)
-     nifticmd = '%s -verbose -mcs %s -pop -origin 0x0x0mm -spacing 0.008x0.008x0.008mm -omc %s' %(c3dexe,pngfile ,options.outimage)
+     os.system(pngcmd)
+     nifticmd = '%s -verbose -mcs %s -pop -origin 0x0x0mm -spacing %fx%fx1mm -omc %s' %(c3dexe,pngfile,spacing ,spacing  ,options.outimage)
      print nifticmd 
+     os.system(nifticmd)
 
      #try:
      #except Exception as excp:
--- PathologyLandmarks/lm.makefile
+++ PathologyLandmarks/lm.makefile
@@ -37,8 +37,10 @@ pathology.lmreg.nii.gz: landmarktransform.tfm pathology.nii.gz dce.nii.gz
 
 # compute lm transformation
 %/t2HElmtransform.tfm: %/Pathology/PathHELM.nii.gz %/Pathology/PathHE.nii.gz %/T2wReference/RefImgLM.nii.gz %/T2wReference/RefImg.hdr
-	$(ITKSNAP) -l LMLabels.txt -s $(word 1, $^)  -g $(word 2, $^)  
-	$(ITKSNAP) -l LMLabels.txt -s $(word 3, $^)  -g $(word 4, $^)  
+	vglrun itksnap -l LMLabels.txt -s $(word 1, $^)  -g $(word 2, $^)  &  PIDPATH=$$!; \
+        vglrun itksnap -l LMLabels.txt -s $(word 3, $^)  -g $(word 4, $^)  &  PIDMRI=$$!; \
+        zenity --info --title="OutputFile" --text="$(word 2, $^) $(word 4, $^)"; \
+        kill -9 $$PIDPATH;kill -9 $$PIDMRI;
 	$(ANTSLANDMARKCMD) $^  rigid $@  >> $(basename $@).log 2>&1
 
 #Convert svs to nifti using open slide to read header
@@ -46,10 +48,6 @@ pathology.lmreg.nii.gz: landmarktransform.tfm pathology.nii.gz dce.nii.gz
 # FIXME - need script to parse header
 %/PathHE.nii.gz: %/PathHE.svs
 	$(CONVERTSVS) --svsfile=$< --outimage=$@
-	openslide-show-properties $<   | grep -i 'layer\[2\|mpp'
-	openslide-write-png $< 0 0 2 2738 2286 pathology.png
-	# remove alpha channel from rgba image and write rgb nii.gz
-	c3d -verbose -mcs pathology.png -pop -origin 0x0x0mm -spacing 0.008x0.008x0.008mm -omc $@
 #aperio.MPP: '0.5027'
 #44704x36680 [0,100 43823x36580] (240x240) J2K/KDU Q=70|AppMag = 20|StripeWidth = 2032|ScanScope ID = SS7063|Filename = 72348|Date = 09/07/16|Time = 15:17:46|Time Zone = GMT-07:00|User = 1a5d2360-04a8-4058-a773-a454a6657563|MPP = 0.5027|Left = 19.602404|Top = 21.335609|LineCameraSkew = 0.000917|LineAreaXOffset = 0.020941|LineAreaYOffset = 0.005602|Focus Offset = -0.000500|DSR ID = DCPWPAPERIO|ImageID = 72348|Exposure Time = 32|Exposure Scale = 0.000001|DisplayColor = 0|OriginalWidth = 44704|OriginalHeight = 36680|ICC Profile = ScanScope v1'
 #openslide.layer[2].downsample: '16.003614116350867'
